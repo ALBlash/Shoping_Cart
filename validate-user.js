adbaa438cbcb104771f1bfa54d91f1183d429904
Ari Blashnikov <119945115+ALBlash@users.noreply.github.com>
2023-08-31 15:47:05 +0300

    first commit

---
+++ validate-user.js
@@ -0,0 +1,23 @@
+function isUserLoggedIn() {
+    // truth or false
+    return !!sessionStorage.getItem("loggedInUserEmail");
+}
+
+if (!isUserLoggedIn()) {
+    window.location.href = "login.html";
+}
+
+function initSession() {
+    // we get the user's (who is logged in) email
+    const loggedInUserEmail = sessionStorage.getItem("loggedInUserEmail");
+    // we get the array of users with all the information
+    const users = JSON.parse(sessionStorage.getItem("users"));
+    // we do the "find" loop and check which key of user's (the key is the email address of the users) so we can confirm the user and take out it's fullname
+    const loggedInUser = users.find(user => user[0] === loggedInUserEmail)[1];
+    document.getElementById('welcome-username').innerText = `welcome ${loggedInUser.fullname}`
+}
+
+function logout() {
+    sessionStorage.removeItem('loggedInUserEmail');
+    window.location.href = "login.html";
+}
